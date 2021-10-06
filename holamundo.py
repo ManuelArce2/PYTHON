72b7f990330d3a9a1ab2825fe5b6b2ab41a17d07
JoseManuelArceSal <91504311+JoseManuelArceSal@users.noreply.github.com>
2021-10-05 22:35:38 -0500

    Hola mundo en python
    primer Hola mundo en python tarde peros seguro

---
+++ holamundo.py
@@ -0,0 +1,8 @@
+#Hola mundo en Phython
+#Title       :Hola Mundo en Python
+#Descripcion :Hola Mundo en python desde vim
+#Autor       :Tec. Jose Manuel Arce Salvador
+#Date        :20211005
+#Version     :1
+#Usage       :C:\>python/path/HolaMundo.py
+print("Hola mundo en pyton JM")
